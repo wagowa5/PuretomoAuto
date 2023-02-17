96e33df36fe7ee0f3954cb996fd111031c2283b6
wagowa5 <82742482+wagowa5@users.noreply.github.com>
2023-02-17 12:45:30 +0900

    ログアウトとざっくりエラーハンドリングを追加

--- puretomoAuto.py
+++ puretomoAuto.py
@@ -7,6 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.chrome.service import Service
+
+# seleniumエラー関係
+#from selenium.common.exceptions import exceptions
+#.UnexpectedAlertPresentException
 # ↓Windowsのとき(もしかしたらなくても動くかも)
 #import chromedriver_binary
 #from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -31,10 +35,12 @@ class Puretomo():
 
         # Mac
         self.driver = webdriver.Chrome()
+        self.driverWait = WebDriverWait(self.driver, 10)
         # Windows(Windowsも↑で動きそう)
         #self.driver = webdriver.Chrome('chromedriver.exe')
         
         print('Chrome init end')
+        printRuledLine()
 
     def __del__(self):
         self.driver.quit()
@@ -45,31 +51,71 @@ class Puretomo():
         self.driver.quit()
         print('Chrome quit')
 
+    # hangeログアウト
+    def hange_logout(self):
+        print('ログアウト開始')
+        # マイページに遷移
+        self.driver.get('https://puretomo.hange.jp/')
+        time.sleep(0.5)
+        
+        # ヘッダーのメニューボタンをクリック
+        self.driver.find_element(By.CSS_SELECTOR, '#__next > div.Header_container__1RawY.undefined > header > nav > div.GlobalHeader_buttonWrap__2ro_f.GlobalHeader_login__3iuxy > a > svg > g').click()
+        
+        # ログアウトボタンをクリック
+        self.driverWait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#__next > div.GlobalModalArea_GlobalModalArea__Uo-TC.GlobalModalArea_lightMode__16WtT > div > div > div.UserInfoMenu_LogoutBtn__1tqPf > button')))
+        self.driver.find_element(By.CSS_SELECTOR, '#__next > div.GlobalModalArea_GlobalModalArea__Uo-TC.GlobalModalArea_lightMode__16WtT > div > div > div.UserInfoMenu_LogoutBtn__1tqPf > button').click()
+        
+        time.sleep(0.5)
+        print('ログアウト完了')
+        printRuledLine()
 
     # hangeログイン
     def hange_login(self, inputId, inputPass):
         print('ログイン開始')
-        self.driver.get('https://top.hange.jp/login/?nexturl=https%3A%2F%2Fwww.hange.jp%2F')
+        try:
+            self.driver.get('https://top.hange.jp/login/?nexturl=https%3A%2F%2Fwww.hange.jp%2F')
 
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#loginform > a.loginBtn')))
+            self.driverWait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#loginform > a.loginBtn')))
 
-        loginId = self.driver.find_element(By.CSS_SELECTOR, '#strmemberid')
-        loginPass = self.driver.find_element(By.CSS_SELECTOR, '#strpassword')
-        loginButton = self.driver.find_element(By.CSS_SELECTOR, '#loginform > a.loginBtn')
+            loginId = self.driver.find_element(By.CSS_SELECTOR, '#strmemberid')
+            loginPass = self.driver.find_element(By.CSS_SELECTOR, '#strpassword')
+            loginButton = self.driver.find_element(By.CSS_SELECTOR, '#loginform > a.loginBtn')
 
-        loginId.send_keys(inputId)
-        loginPass.send_keys(inputPass)
+            loginId.send_keys(inputId)
+            loginPass.send_keys(inputPass)
 
-        loginButton.click()
+            loginButton.click()
+            
+            time.sleep(0.2)
+            # ログインに問題がないかマイページへの遷移で確認する
+            self.driver.get('https://puretomo.hange.jp/closet/')
+        except Exception as e:
+            print('ログインエラー。id,パスワードを確認して再度お試しください')
+            print(e)
+
+            # ログイン再開できるようにログインページに戻る
+            self.driver.get('https://top.hange.jp/login/?nexturl=https%3A%2F%2Fwww.hange.jp%2F')
+            return False
+        
         print('ログイン完了')
+        printRuledLine()
+        return True
 
-        # puretomo着替えページに遷移
+    # puretomo マネキン一覧画面に遷移
+    def transition_puretomo_mannequin(self):
         print('puretomo着替えページへ遷移開始')
         self.driver.get('https://puretomo.hange.jp/closet/')
         # マイページに遷移することがあるため1秒待機してリトライ
         time.sleep(1)
         self.driver.get('https://puretomo.hange.jp/closet/')
+
+        # マネキンボタンをクリック
+        print('着替えページを表示, マネキン一覧へ遷移します')
+        self.driverWait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_mainTab__1fA0U > div:nth-child(6) > button')))
+        mannequinBtn = self.driver.find_element(By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_mainTab__1fA0U > div:nth-child(6) > button')
+        mannequinBtn.click()
+        print('マネキン一覧への遷移完了')
+        printRuledLine()
 
         # 新規driverではアラートが表示されるため4秒待機して表示させた後に拒否する
         # ↓アラートのセレクタもXPATHもclass_nameもno such elementで取得できなかった
@@ -78,22 +124,6 @@ class Puretomo():
         #self.driver.find_element(by=By.CLASS_NAME, value="deny")
         #wait.until(EC.alert_is_present())
         #Alert(self.driver).deny()
-
-        print('着替えページを表示, マネキン一覧へ遷移します')
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_mainTab__1fA0U > div:nth-child(6) > button')))
-        mannequinBtn = self.driver.find_element(By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_mainTab__1fA0U > div:nth-child(6) > button')
-        mannequinBtn.click()
-        print('マネキン一覧への遷移完了')
-
-
-    # puretomo 着替え画面に遷移
-    def transition_puretomo_mannequin(self):
-        self.driver.get('https://puretomo.hange.jp/closet/')
-
-        # マネキンをクリック
-        mannequinBtn = self.driver.find_element(By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_mainTab__1fA0U > div:nth-child(6) > button')
-        mannequinBtn.click()
-
 
     # マネキン名一覧取得
     def get_mannequins(self):
@@ -124,6 +154,7 @@ class Puretomo():
                     mannequinNames.append(mannequinName)
 
         print('マネキン名取得完了')
+        printRuledLine()
 
         # 着替え時に前から検索できるように先頭ページに遷移しておく
         firstPageButton = self.driver.find_element(By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_itemListMainAreaWrap__esqU- > div.pager > nav > ul > li:nth-child(2) > button')
@@ -134,7 +165,6 @@ class Puretomo():
     
     # マネキン着替え保存
     def change_mannequin(self, changeMannequinName, inputId):
-        print('マネキン着替え開始')
         # ページ数を取得
         page = self.driver.find_elements(By.CSS_SELECTOR, '#closetAreaWrap > div.areaWrap > div.closet_itemBagArea__hcoGs > div.closet_itemListMainAreaWrap__esqU- > div.pager > nav > ul > li')
         pageNum = len(page)
@@ -188,6 +218,9 @@ class Puretomo():
             firstPageButton.click()
 
 
+def printRuledLine():
+    print('----------------------------')
+
 # マネキンを選択して着替えられるウィンドウを表示
 def mannequinChangeWindow(chrome, mannequinNames, inputId):
     # ログインフラグ
@@ -199,13 +232,12 @@ def mannequinChangeWindow(chrome, mannequinNames, inputId):
     layout = [[sg.Text('login_id: ',size=(12,1)), sg.InputText(password_char="*")],
             [sg.Text('login_pass: ',size=(12,1)), sg.InputText(password_char="*")],
             [sg.Button('ログイン'), sg.Button('マネキン取得'), sg.Button('終了')],
-            [sg.Text('ーーーーーーーーーーーーーー')],
+            [sg.Text('----------------------------')],
             [sg.Text('マネキン一覧')]
             ]
-  
+
     # マネキンボタンを追加
     addLayout = []
-    print(mannequinNames)
     for buttonName in mannequinNames:
         addLayout.append(sg.Button(buttonName, size=(10,1)))
         if len(addLayout) == 5:
@@ -214,8 +246,6 @@ def mannequinChangeWindow(chrome, mannequinNames, inputId):
     if addLayout != []:
         layout.append(addLayout)
 
-    print(layout)
-
     # ウィンドウの生成
     changeWindow = sg.Window('puretomoマネキン自動着替え(非公式)', layout)
 
@@ -223,27 +253,34 @@ def mannequinChangeWindow(chrome, mannequinNames, inputId):
     while True:
         event, values = changeWindow.read()
         if event == sg.WIN_CLOSED or event == '終了':
+            if isLogin:
+                chrome.hange_logout()
             chrome.quit()
             break
         elif event == 'ログイン':
             if isLogin:
-                # TODO 画面に表示
                 print('既にログインしています')
             else:
-                chrome.hange_login(str(values[0]), str(values[1]))
-                isLogin = True
+                # TODO ここには入らない, いつか整理する
+                print('ここには来ない')
         elif event == 'マネキン取得':
             if isLogin:
-                # TODO たたけないようにする
-                print('tukawanai')
+                print('既に取得しています')
             else:
-                # TODO 画面に表示
+                # TODO ここには入らない, いつか整理する
                 print('ログインしてください')
         elif event in mannequinNames:
             print('「' + str(event) + '」に着替えます')
-            chrome.change_mannequin(str(event), inputId)
-            # 連続して着替えすぎると失敗するためスリープ
-            time.sleep(1)
+            try:
+                chrome.change_mannequin(str(event), inputId)
+                # 連続して着替えすぎると失敗するためスリープ
+                time.sleep(1)
+            except:
+                print('マネキン着替え保存でエラー')
+                print('短時間に着替えすぎるとサイトが追いつかず失敗することがあります')
+                print(e)
+            printRuledLine()
+            
     changeWindow.close()
 
 
@@ -271,19 +308,34 @@ def main():
     # イベントループ
     while True:
         event, values = window.read()
+        
         if event == sg.WIN_CLOSED or event == '終了':
+            if isLogin:
+                chrome.hange_logout()
             chrome.quit()
             break
+        
         elif event == 'ログイン':
             if isLogin:
-                # TODO 画面に表示
                 print('既にログインしています')
             else:
-                chrome.hange_login(str(values[0]), str(values[1]))
-                isLogin = True
+                isLogin = chrome.hange_login(str(values[0]), str(values[1]))
+                if isLogin:
+                    try:
+                        chrome.transition_puretomo_mannequin()
+                    except:
+                        print('puretomoページへの遷移に失敗')
         elif event == 'マネキン取得':
             if isLogin:
-                mannequinNames = chrome.get_mannequins()
+                try:
+                    mannequinNames = chrome.get_mannequins()
+                except:
+                    print('マネキン取得でエラー。')
+                    print('chromeにダイアログが表示されている場合は拒否して再度お試しください')
+                    print(e)
+
+                    # Puretomoマネキンページに戻ってリトライできるようにする
+                    chrome.transition_puretomo_mannequin()
                 if len(mannequinNames) != 0:
                     window.Hide()
                     # マネキン着替え用画面を表示
@@ -291,7 +343,6 @@ def main():
                     window.UnHide()
                     break
             else:
-                # TODO 画面に表示
                 print('ログインしてください')
 
     window.close()
